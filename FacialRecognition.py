20e3d17cb1f6eacf6966f7e0fb724fecc55f1f1a
arnabdey <arnabdey29@gmail.com>
2018-03-10 16:37:14 -0500

    Updated treshold level

--- FacialRecognition.py
+++ FacialRecognition.py
@@ -69,6 +69,9 @@ while (repeat == ""):
         else:
             print("Showing Image of Person")
             person  = cv2.imread("practice.png")
+            f = open('output.txt', 'w')
+            f.write("A person is appearing")
+            f.close()
     #        cv2.imshow("image", person)
     #        cv2.waitKey(0)
     surroundingsChanged = False
