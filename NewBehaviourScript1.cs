ddccf214dfc19ab36333d60e340bc3c0db2a8a71
arnabdey <arnabdey29@gmail.com>
2018-03-10 20:00:38 -0500

    Adding C# code

---
+++ NewBehaviourScript1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class NewBehaviourScript1 : MonoBehaviour
+{
+
+    public Font font;
+
+    private void Update()
+    {
+        Debug.Log("running");
+        string path = "C:/Users/MLH/Documents/VR Test 1/Assets/Resources/output.txt";
+        StreamReader reader = new StreamReader(path);
+        string input = reader.ReadToEnd();
+        reader.Close();
+        if (input.Length > 0)
+        {
+            GetComponent<TextMesh>().text = "Someone is behind you";
+            Debug.Log("debugging");
+        } else
+        {
+            GetComponent<TextMesh>().text = " ";
+        }
+    }
+}
+
